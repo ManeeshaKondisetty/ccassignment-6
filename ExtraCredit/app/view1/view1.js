66aef1197874555d1ff63101ca38cbed24bcf9a7
ManeeshaKondisetty <mpk5fd@umsystem.edu>
2021-05-15 23:05:25 -0500

    Update angular

--- ExtraCredit/app/view1/view1.js
+++ ExtraCredit/app/view1/view1.js
@@ -1,3 +1,18 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', [function() {
+
+}]);
+
 function getData() {
   var firstCar = document.getElementById('one_cars').value;
   var secondCar = document.getElementById('two_cars').value;
@@ -151,3 +166,4 @@ var Barchart = function(options){
     }
   }
 }
+
